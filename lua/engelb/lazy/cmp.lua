54c17c1fb120817c19e47d7df01f6678a962d0f8
Baptiste Engel <baptiste.engel@cea.fr>
2026-01-08 17:53:49 +0100

    Updated LuaSnip config in cmp condif

--- lua/engelb/lazy/cmp.lua
+++ lua/engelb/lazy/cmp.lua
@@ -7,11 +7,12 @@ return {
         "L3MON4D3/LuaSnip",     -- Snippet engine
         "saadparwaiz1/cmp_luasnip", -- Bridge between cmp and luasnip
         "hrsh7th/cmp-buffer",   -- Source for buffer text completions
+"rafamadriz/friendly-snippets",
     },
     config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
-
+        require("luasnip.loaders.from_vscode").lazy_load()
         cmp.setup({
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
@@ -29,6 +30,27 @@ return {
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.abort(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+
+                -- Shift-Tab mapping
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
